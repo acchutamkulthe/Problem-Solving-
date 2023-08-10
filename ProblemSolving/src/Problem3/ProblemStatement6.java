5d76c97efbe99bfff0e4a1de08222d2423ac1881
Acchutam <acchutam.kulthe@neosoftmail.com>
2023-08-10 11:46:58 +0530

    ProblemStatement6

---
+++ ProblemSolving/src/Problem3/ProblemStatement6.java
@@ -0,0 +1,40 @@
+package Problem3;
+
+public class ProblemStatement6 {
+
+    public static void divisible(int num){
+            int temp,quotient=num,count=0;
+            while(quotient!=0){
+                temp=quotient%10;
+                if(num%temp==0){
+                    count++;
+                }
+                quotient/=10;
+            }
+        System.out.println(count);
+    }
+    public static void main(String[] args) {
+        divisible(12);
+    }
+}
+/*Given a number N. Count the number of digits in N which evenly divides N.
+
+Note :- Evenly divides means whether N is divisible by a digit i.e. leaves a remainder 0 when divided.
+
+Example 1:
+
+Input:
+N = 12
+Output:
+2
+Explanation:
+1, 2 both divide 12 evenly
+Example 2:
+
+Input:
+N = 23
+Output
+0
+Explanation:
+2 and 3, none of them
+divide 23 evenly*/
